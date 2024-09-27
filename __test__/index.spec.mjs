f07d386f90e8af903b0764478b1e7b3077ec2bcb
Eko J. Salim <eko@ekojs.com>
2024-09-28 01:36:46 +0700

    fix: tests

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,47 @@
-import test from 'ava'
+import test from "ava";
 
-import { sum } from '../index.js'
+import {
+  Automaton,
+  MATCH_KIND_STANDARD,
+  MATCH_KIND_LEFTMOST_LONGEST,
+  MATCH_KIND_LEFTMOST_FIRST,
+  createAutomaton,
+} from "../index.js";
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
-})
+test("Automaton", (t) => {
+  // standard match
+  const patternsStandard = ["bcd", "ab", "a"];
+  const automatonStandard = new Automaton(
+    patternsStandard,
+    MATCH_KIND_STANDARD
+  );
+
+  t.deepEqual(automatonStandard.find("abcd"), ["a", "bcd"]);
+
+  // leftmost longest match
+  const patternsLeftmostLongest = ["ab", "a", "abcd"];
+  const automatonLeftmostLongest = new Automaton(
+    patternsLeftmostLongest,
+    MATCH_KIND_LEFTMOST_LONGEST
+  );
+
+  t.deepEqual(automatonLeftmostLongest.find("abcd"), ["abcd"]);
+
+  // Test leftmost first match
+  const patternsLeftmostFirst = ["ab", "a", "abcd"];
+  const automatonLeftmostFirst = new Automaton(
+    patternsLeftmostFirst,
+    MATCH_KIND_LEFTMOST_FIRST
+  );
+
+  t.deepEqual(automatonLeftmostFirst.find("abcd"), ["ab"]);
+
+  // createAutomaton
+  const patternsCreateAutomaton = ["ab", "a", "abcd"];
+  const automatonCreateAutomaton = createAutomaton(
+    patternsCreateAutomaton,
+    MATCH_KIND_LEFTMOST_LONGEST
+  );
+
+  t.deepEqual(automatonCreateAutomaton.find("abcd"), ["abcd"]);
+});
